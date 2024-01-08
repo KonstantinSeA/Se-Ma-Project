a73b68e73d20071c1cb5a2f074ab8e2e21c0073c
KonstantinSeA <Acidvein@yandex.ru>
2024-01-08 14:24:25 +0300

    Обновление Спрайтов Травы, общее изменение размера тайлов на 25

--- plays.py
+++ plays.py
@@ -28,12 +28,12 @@ class Tile(pygame.sprite.Sprite):
             super().__init__(all_sprites, tile_group)
             self.image = load_image(f'Grass_{self.tile[2] + self.tile[1]}.jpg', 'Sprites/Grass')
             self.rect = self.image.get_rect()
-            self.rect.x, self.rect.y = 50 * pos_x, 50 * pos_y
+            self.rect.x, self.rect.y = 25 * pos_x, 25 * pos_y
         if self.tile[0] == 'Water':
             super().__init__(all_sprites, tile_group, d_group)
             self.image = load_image(f'Water_{self.tile[2] + self.tile[1]}.jpg', 'Sprites/Water')
             self.rect = self.image.get_rect()
-            self.rect.x, self.rect.y = 50 * pos_x, 50 * pos_y
+            self.rect.x, self.rect.y = 25 * pos_x, 25 * pos_y
 
     def update(self, arg):
         if arg == 'step':
@@ -72,11 +72,11 @@ class Inventory:
 class Hero(pygame.sprite.Sprite):
     def __init__(self, v):
         super().__init__(all_sprites, hero_group)
-        self.image = pygame.Surface((40, 60), pygame.SRCALPHA, 32)
+        self.image = pygame.Surface((20, 45), pygame.SRCALPHA, 32)
         if v == 0:
             self.image = load_image('Character_00.png', 'Sprites/Hero/Character')
         elif v == 1:
-            pygame.draw.rect(self.image, pygame.Color('magenta'), (0, 0, 40, 60))
+            pygame.draw.rect(self.image, pygame.Color('magenta'), (0, 0, 20, 45))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 25, 25
         self.ori = 'r'
