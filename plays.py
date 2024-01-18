411b10b1012339354995b7665294f5df2c2e0873
KonstantinSeA <Acidvein@yandex.ru>
2024-01-18 21:23:01 +0300

    Полное обновление архитектуры игры, компановка игры в один класс, переработка столкновения персонажа персонажа, и домик сделаль

--- plays.py
+++ plays.py
@@ -6,21 +6,6 @@ FPS = 60
 small_grow_tile = []
 
 
-class Game:
-    def __init__(self, save):
-        self.save = save
-        set_map(load_map(f'Saves/Save{self.save}/Map.txt'))
-        with open(f'Saves/Save{self.save}/Save.txt', mode='r', encoding='utf-8') as save_file:
-            data_lines = save_file.readlines()
-        self.character = Character(0, f'Saves/Save{self.save}/Inv.txt')
-        self.game_clock = GameClock(data_lines[2].split('; ')[0], data_lines[2].split('; ')[1])
-        self.time_update = pygame.USEREVENT + 1
-        pygame.time.set_timer(self.time_update, 7000)
-
-    def next_day(self):
-        pass
-
-
 def load_image(name, road, colorkey=None):
     fullname = os.path.join(road, name)
     if not os.path.isfile(fullname):
@@ -41,12 +26,12 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, tile, pos_x, pos_y):
         self.tile = tile.split(';')
         if self.tile[0] == 'Grass':
-            super().__init__(all_sprites, tile_group, g_group)
+            super().__init__(game.all_sprites, game.tile_group, game.g_group)
             self.image = load_image(f'Grass_{self.tile[2] + self.tile[1]}.jpg', 'Sprites/Grass')
             self.rect = self.image.get_rect()
             self.rect.x, self.rect.y = 25 * pos_x + 75, 25 * pos_y + 140
         if self.tile[0] == 'Water':
-            super().__init__(all_sprites, tile_group, d_group)
+            super().__init__(game.all_sprites, game.tile_group, game.d_group)
             self.image = load_image(f'Water_{self.tile[2] + self.tile[1]}.jpg', 'Sprites/Water')
             self.rect = self.image.get_rect()
             self.rect.x, self.rect.y = 25 * pos_x + 80, 25 * pos_y + 80
@@ -87,7 +72,7 @@ def set_map(map_level):
 
 class Item(pygame.sprite.Sprite):
     def __init__(self, item_name, pos):
-        super().__init__(all_sprites, menu_group)
+        super().__init__(game.all_sprites, game.menu_group)
         self.name, self.pos = item_name, pos
         self.image = load_image(f'{item_name}.png', f'Sprites/Items')
         self.rect = self.image.get_rect()
@@ -98,7 +83,7 @@ class Item(pygame.sprite.Sprite):
 
     def use(self, ori, pos):
         point = MyPoint(ori, pos)
-        used_tile = pygame.sprite.spritecollide(point, all_sprites, False)
+        used_tile = pygame.sprite.spritecollide(point, game.all_sprites, False)
         if used_tile:
             if self.name == 'wood_shovel':
                 used_tile[0].update('uws')
@@ -109,7 +94,7 @@ class Item(pygame.sprite.Sprite):
 
 class MyPoint(pygame.sprite.Sprite):
     def __init__(self, ori, pos):
-        super().__init__(all_sprites, worked_group)
+        super().__init__(game.all_sprites, game.worked_group)
         self.image = pygame.Surface((1, 1), pygame.SRCALPHA, 32)
         pygame.draw.rect(self.image, pygame.Color('magenta'), (0, 0, 1, 1))
         self.rect = self.image.get_rect()
@@ -133,8 +118,8 @@ class Inventory:
 
 
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, v):
-        super().__init__(all_sprites, hero_group)
+    def __init__(self, v, inv):
+        super().__init__(game.all_sprites, game.hero_group)
         self.image = pygame.Surface((20, 45), pygame.SRCALPHA, 32)
         if v == 0:
             self.image = load_image('Character_00.png', 'Sprites/Hero/Character')
@@ -144,6 +129,9 @@ class Hero(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = 500, 400
         self.absolute_x, self.absolute_y = 500, 400
         self.ori = 'r'
+        self.inv = Inventory(inv)
+        self.shoosed_item = 1
+        self.boots = HeroBoots(self)
 
     def update(self, arg):
         if arg[0] == 'm':
@@ -151,57 +139,74 @@ class Hero(pygame.sprite.Sprite):
                 self.ori = 'u'
                 self.rect.y -= 5
                 self.absolute_y -= 5
-                if pygame.sprite.groupcollide(hero_group, d_group, False, False):
+                self.boots.update(self)
+                if pygame.sprite.groupcollide(game.hero_boots_group, game.d_group, False, False)\
+                        or mask_collide(self.boots, game.d_mask_group):
                     self.rect.y += 5
                     self.absolute_y += 5
             elif arg[1] == 'l':
                 self.ori = 'l'
                 self.rect.x -= 5
                 self.absolute_x -= 5
-                if pygame.sprite.groupcollide(hero_group, d_group, False, False):
+                self.boots.update(self)
+                if pygame.sprite.groupcollide(game.hero_boots_group, game.d_group, False, False)\
+                        or mask_collide(self.boots, game.d_mask_group):
                     self.rect.x += 5
                     self.absolute_x += 5
             elif arg[1] == 'd':
                 self.ori = 'd'
                 self.rect.y += 5
                 self.absolute_y += 5
-                if pygame.sprite.groupcollide(hero_group, d_group, False, False):
+                self.boots.update(self)
+                if pygame.sprite.groupcollide(game.hero_boots_group, game.d_group, False, False)\
+                        or mask_collide(self.boots, game.d_mask_group):
                     self.rect.y -= 5
                     self.absolute_y -= 5
             elif arg[1] == 'r':
                 self.ori = 'r'
                 self.rect.x += 5
                 self.absolute_x += 5
-                if pygame.sprite.groupcollide(hero_group, d_group, False, False):
+                self.boots.update(self)
+                if pygame.sprite.groupcollide(game.hero_boots_group, game.d_group, False, False)\
+                        or mask_collide(self.boots, game.d_mask_group):
                     self.rect.x -= 5
                     self.absolute_x -= 5
+            elif arg == 'da':
+                self.inv.items[self.shoosed_item - 1].use(self.ori, (self.rect.x, self.rect.y))
+            elif arg == '1':
+                self.shoosed_item = 1
+            elif arg == '2':
+                self.shoosed_item = 2
+            elif arg == '3':
+                self.shoosed_item = 3
+            elif arg == '4':
+                self.shoosed_item = 4
+            elif arg == '5':
+                self.shoosed_item = 5
+            print(self.rect.x, self.rect.y, '----', self.absolute_x, self.absolute_y)
+        if pygame.sprite.spritecollideany(self.boots, game.home_t_group):
+            print(1)
+        self.boots.update(self)
 
 
-class NPC(Hero):
-    ...
+def mask_collide(sprite, sprite_group):
+    for s in sprite_group:
+        if pygame.sprite.collide_mask(sprite, s):
+            return True
+    return False
 
 
-class Character(Hero):
-    def __init__(self, v, inv):
-        super().__init__(v)
-        self.inv = Inventory(inv)
-        self.shoosed_item = 1
+class HeroBoots(pygame.sprite.Sprite):
+    def __init__(self, hero):
+        super().__init__(game.all_sprites, game.hero_boots_group)
+        self.image = pygame.Surface((20, 15), pygame.SRCALPHA, 32)
+        pygame.draw.rect(self.image, pygame.Color('magenta'), (0, 0, 20, 15))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = hero.rect.x, hero.rect.y + 30
+        self.mask = pygame.mask.from_surface(self.image)
 
-    def update(self, arg):
-        super().update(arg)
-        if arg == 'da':
-            self.inv.items[self.shoosed_item - 1].use(self.ori, (self.rect.x, self.rect.y))
-        elif arg == '1':
-            self.shoosed_item = 1
-        elif arg == '2':
-            self.shoosed_item = 2
-        elif arg == '3':
-            self.shoosed_item = 3
-        elif arg == '4':
-            self.shoosed_item = 4
-        elif arg == '5':
-            self.shoosed_item = 5
-        print(self.rect.x, self.rect.y, '----', self.absolute_x, self.absolute_y)
+    def update(self, hero):
+        self.rect.x, self.rect.y = hero.rect.x, hero.rect.y + 30
 
 
 class GameClock:
@@ -238,14 +243,14 @@ class Camera:
             obj.rect = obj.rect.move(0, self.dy)
 
     def update(self, target):
-        self.dx = width // 2 - target.rect.x - target.rect.w // 2
-        self.dy = height // 2 - target.rect.y - target.rect.h // 2
+        self.dx = game.width // 2 - target.rect.x - target.rect.w // 2
+        self.dy = game.height // 2 - target.rect.y - target.rect.h // 2
         self.absolute_x, self.absolute_y = target.absolute_x, target.absolute_y
 
 
 class FonWall(pygame.sprite.Sprite):
     def __init__(self, t):
-        super().__init__(all_sprites, d_group, wall_group)
+        super().__init__(game.all_sprites, game.d_group, game.wall_group)
         if t == 'l':
             self.image = pygame.Surface((45, 1400), pygame.SRCALPHA, 32)
             pygame.draw.line(self.image, 'red', (0, 0), (0, 1400), 45)
@@ -257,7 +262,7 @@ class FonWall(pygame.sprite.Sprite):
             self.rect = self.image.get_rect()
             self.rect.x, self.rect.y = 1240, 0
         if t == 'u':
-            self.image = pygame.Surface((1400, 95), pygame.SRCALPHA, 32)
+            self.image = pygame.Surface((1400, 125), pygame.SRCALPHA, 32)
             pygame.draw.line(self.image, 'red', (0, 0), (1400, 0), 125)
             self.rect = self.image.get_rect()
             self.rect.x, self.rect.y = 0, 0
@@ -265,17 +270,17 @@ class FonWall(pygame.sprite.Sprite):
             self.image = pygame.Surface((1400, 5), pygame.SRCALPHA, 32)
             pygame.draw.line(self.image, 'red', (0, 0), (1400, 0), 5)
             self.rect = self.image.get_rect()
-            self.rect.x, self.rect.y = 0, 980
+            self.rect.x, self.rect.y = 0, 1005
         if t == 'vu':
-            self.image = pygame.Surface((600, 130), pygame.SRCALPHA, 32)
-            pygame.draw.line(self.image, 'red', (0, 0), (600, 0), 125)
+            self.image = pygame.Surface((600, 150), pygame.SRCALPHA, 32)
+            pygame.draw.line(self.image, 'red', (0, 0), (600, 0), 150)
             self.rect = self.image.get_rect()
             self.rect.x, self.rect.y = 800, 0
 
 
 class Fon(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__(all_sprites, tile_group)
+        super().__init__(game.all_sprites, game.tile_group)
         self.image = load_image(f'Farm_Fon.png', 'Sprites')
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = 0, 0
@@ -286,62 +291,136 @@ class Fon(pygame.sprite.Sprite):
         FonWall('vu')
 
 
-pygame.init()
-all_sprites = pygame.sprite.Group()
-tile_group = pygame.sprite.Group()
-hero_group = pygame.sprite.Group()
-d_group = pygame.sprite.Group()
-g_group = pygame.sprite.Group()
-menu_group = pygame.sprite.Group()
-worked_group = pygame.sprite.Group()
-fon_group = pygame.sprite.Group()
-wall_group = pygame.sprite.Group()
-Fon()
-size = width, height = 1000, 800
-screen = pygame.display.set_mode(size)
-game = Game('a')
-clock = pygame.time.Clock()
-running = True
-camera = Camera()
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
-            hero_group.update('da')
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-            hero_group.update('1')
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_2:
-            hero_group.update('2')
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_3:
-            hero_group.update('3')
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_4:
-            hero_group.update('4')
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_5:
-            hero_group.update('5')
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_u:
-            hero_group.update('5')
-        if event.type == game.time_update:
-            game.game_clock.time_update()
-    if pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_w]:
-        hero_group.update('mu')
-    if pygame.key.get_pressed()[pygame.K_LEFT] or pygame.key.get_pressed()[pygame.K_a]:
-        hero_group.update('ml')
-    if pygame.key.get_pressed()[pygame.K_DOWN] or pygame.key.get_pressed()[pygame.K_s]:
-        hero_group.update('md')
-    if pygame.key.get_pressed()[pygame.K_RIGHT] or pygame.key.get_pressed()[pygame.K_d]:
-        hero_group.update('mr')
+class Home:
+    def __init__(self):
+        self.roof = HomeRoof()
+        self.wall = HomeWall()
+        self.terrace = HomeTerrace()
+        self.mat = HomeMat()
 
-    screen.fill('white')
-    camera.update(game.character)
-    camera.appply(game.character)
-    for sprite in tile_group:
-        camera.appply(sprite)
-    for sprite in wall_group:
-        camera.appply(sprite)
-    tile_group.draw(screen)
-    hero_group.draw(screen)
-    game.game_clock.draw_time(screen)
-    menu_group.draw(screen)
-    pygame.display.flip()
-    clock.tick(FPS)
+
+class HomeRoof(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(game.all_sprites, game.tile_group, game.walked_group)
+        self.image = load_image(f'Home_Roof.png', 'Sprites/Home')
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 800, 355
+
+
+class HomeMat(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(game.all_sprites, game.tile_group, game.home_t_group)
+        self.image = load_image(f'Home_Mat.png', 'Sprites/Home')
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 910, 507
+
+
+class HomeTerrace(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(game.all_sprites, game.tile_group)
+        self.image = load_image(f'Home_Terrace.png', 'Sprites/Home')
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 801, 505
+
+
+class HomeWall(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(game.all_sprites, game.tile_group, game.d_mask_group)
+        self.image = load_image(f'Home_Wall.png', 'Sprites/Home')
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 800, 355
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+class Game:
+    def __init__(self, save):
+        self.save = save
+
+    def next_day(self):
+        pass
+
+    def run(self):
+        while self.running:
+            if self.run_type == 'farm':
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.running = False
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
+                        self.hero_group.update('da')
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+                        self.hero_group.update('1')
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_2:
+                        self.hero_group.update('2')
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_3:
+                        self.hero_group.update('3')
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_4:
+                        self.hero_group.update('4')
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_5:
+                        self.hero_group.update('5')
+                    if event.type == pygame.KEYDOWN and event.key == pygame.K_u:
+                        self.hero_group.update('5')
+                    if event.type == game.time_update:
+                        self.game_clock.time_update()
+                if pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_w]:
+                    self.hero_group.update('mu')
+                if pygame.key.get_pressed()[pygame.K_LEFT] or pygame.key.get_pressed()[pygame.K_a]:
+                    self.hero_group.update('ml')
+                if pygame.key.get_pressed()[pygame.K_DOWN] or pygame.key.get_pressed()[pygame.K_s]:
+                    self.hero_group.update('md')
+                if pygame.key.get_pressed()[pygame.K_RIGHT] or pygame.key.get_pressed()[pygame.K_d]:
+                    self.hero_group.update('mr')
+
+                self.screen.fill('white')
+                self.camera.update(game.character)
+                self.camera.appply(game.character)
+                for sprite in self.tile_group:
+                    self.camera.appply(sprite)
+                for sprite in self.wall_group:
+                    self.camera.appply(sprite)
+                self.character.boots.update(self.character)
+                self.tile_group.draw(self.screen)
+                self.hero_group.draw(self.screen)
+                self.walked_group.draw(self.screen)
+                self.game_clock.draw_time(self.screen)
+                self.menu_group.draw(self.screen)
+                pygame.display.flip()
+                self.clock.tick(FPS)
+            elif self.run_type == 'home':
+                ...
+
+
+    def init(self):
+        pygame.init()
+        self.all_sprites = pygame.sprite.Group()
+        self.tile_group = pygame.sprite.Group()
+        self.hero_group = pygame.sprite.Group()
+        self.hero_boots_group = pygame.sprite.Group()
+        self.d_group = pygame.sprite.Group()
+        self.g_group = pygame.sprite.Group()
+        self.menu_group = pygame.sprite.Group()
+        self.worked_group = pygame.sprite.Group()
+        self.fon_group = pygame.sprite.Group()
+        self.wall_group = pygame.sprite.Group()
+        self.d_mask_group = pygame.sprite.Group()
+        self.walked_group = pygame.sprite.Group()
+        self.home_t_group = pygame.sprite.Group()
+        Fon()
+        self.size = self.width, self.height = 1000, 800
+        self.screen = pygame.display.set_mode(self.size)
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.camera = Camera()
+        set_map(load_map(f'Saves/Save{self.save}/Map.txt'))
+        with open(f'Saves/Save{self.save}/Save.txt', mode='r', encoding='utf-8') as save_file:
+            data_lines = save_file.readlines()
+        self.character = Hero(0, f'Saves/Save{self.save}/Inv.txt')
+        self.run_type = 'farm'
+        self.game_clock = GameClock(data_lines[2].split('; ')[0], data_lines[2].split('; ')[1])
+        self.time_update = pygame.USEREVENT + 1
+        self.home = Home()
+        pygame.time.set_timer(self.time_update, 7000)
+
+
+game = Game('a')
+game.init()
+game.run()
